68cfaf7172005fc5ef51d8183dc0850e3b6d2f3e
anderson_rodriguez <andersonrodriguezce@gmail.com>
2022-06-10 17:15:15 -0500

    addinga a creation route

--- go.mod
+++ go.mod
@@ -5,4 +5,16 @@ go 1.18
 require (
 	github.com/go-chi/chi/v5 v5.0.7
 	github.com/go-chi/render v1.0.1
+	github.com/go-playground/validator/v10 v10.11.0
+	github.com/shurcooL/graphql v0.0.0-20220606043923-3cf50f8a0a29
+)
+
+require (
+	github.com/go-playground/locales v0.14.0 // indirect
+	github.com/go-playground/universal-translator v0.18.0 // indirect
+	github.com/leodido/go-urn v1.2.1 // indirect
+	golang.org/x/crypto v0.0.0-20211215153901-e495a2d5b3d3 // indirect
+	golang.org/x/net v0.0.0-20220607020251-c690dde0001d // indirect
+	golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a // indirect
+	golang.org/x/text v0.3.7 // indirect
 )
--- main.go
+++ main.go
@@ -4,29 +4,45 @@ import (
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/go-chi/render"
+	"github.com/shurcooL/graphql"
+	"log"
 	"net/http"
 	"strconv"
 )
 
 type User struct {
-	UserID   int64  `json:"userID,omitempty"`
-	UserName string `json:"userName,omitempty"`
-	Wildcard string `json:"wildcard,omitempty"`
+	ID       int64  `json:"userID,omitempty" validate:"omitempty"`
+	UserName string `json:"userName,omitempty" validate:"alphanum"`
+	Wildcard string `json:"wildcard,omitempty" validate:"required"`
 }
 
 func (u *User) Render(w http.ResponseWriter, r *http.Request) error {
 	return nil
 }
+func (u *User) Bind(r *http.Request) error {
+	return nil
+}
+
+type Pokemon struct {
+	Name    graphql.String `json:"name,omitempty"`
+	Message graphql.String `json:"message,omitempty"`
+}
+
+func (p *Pokemon) Render(w http.ResponseWriter, r *http.Request) error {
+	return nil
+}
 
 func main() {
+	// HTTP Server
 	router := chi.NewRouter()
 	router.Use(middleware.Logger)
 
+	// Routes
 	router.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello World!"))
 	})
-
 	router.Get("/users/{id}/{username}/*", getUser)
+	router.Post("/users", createUser)
 
 	http.ListenAndServe(":3000", router)
 }
@@ -40,4 +56,15 @@ func getUser(w http.ResponseWriter, r *http.Request) {
 
 	render.Status(r, http.StatusOK)
 	render.Render(w, r, user)
+}
+
+func createUser(w http.ResponseWriter, r *http.Request) {
+	data := &User{}
+	if err := render.Bind(r, data); err != nil {
+		log.Print(err.Error())
+		render.Render(w, r, data)
+		return
+	}
+
+	render.Render(w, r, data)
 }
