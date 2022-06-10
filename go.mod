54f883dd6de42e335b195294bcaef817b061ec71
anderson_rodriguez <andersonrodriguezce@gmail.com>
2022-06-10 15:10:13 -0500

    creating get users endpoint

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module chi-sqlc-getting-started
 
 go 1.18
 
-require github.com/go-chi/chi/v5 v5.0.7
+require (
+	github.com/go-chi/chi/v5 v5.0.7
+	github.com/go-chi/render v1.0.1
+)
--- main.go
+++ main.go
@@ -3,8 +3,20 @@ package main
 import (
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/render"
 	"net/http"
+	"strconv"
 )
+
+type User struct {
+	UserID   int64  `json:"userID,omitempty"`
+	UserName string `json:"userName,omitempty"`
+	Wildcard string `json:"wildcard,omitempty"`
+}
+
+func (u *User) Render(w http.ResponseWriter, r *http.Request) error {
+	return nil
+}
 
 func main() {
 	router := chi.NewRouter()
@@ -14,5 +26,18 @@ func main() {
 		w.Write([]byte("Hello World!"))
 	})
 
+	router.Get("/users/{id}/{username}/*", getUser)
+
 	http.ListenAndServe(":3000", router)
+}
+
+func getUser(w http.ResponseWriter, r *http.Request) {
+	userID, _ := strconv.ParseInt(chi.URLParam(r, "id"), 10, 32)
+	userName := chi.URLParam(r, "username")
+	wildcard := chi.URLParam(r, "*")
+
+	user := &User{userID, userName, wildcard}
+
+	render.Status(r, http.StatusOK)
+	render.Render(w, r, user)
 }
