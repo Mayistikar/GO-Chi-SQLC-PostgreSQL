00ba1a8a758f9942158c7c3b1eb26d162000a441
anderson_rodriguez <andersonrodriguezce@gmail.com>
2022-06-12 17:55:09 -0500

    adding a graphql consulting

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/go-chi/render"
@@ -23,12 +24,13 @@ func (u *User) Bind(r *http.Request) error {
 	return nil
 }
 
-type Pokemon struct {
-	Name    graphql.String `json:"name,omitempty"`
-	Message graphql.String `json:"message,omitempty"`
+type Character struct {
+	ID     graphql.ID     `json:"id,omitempty"`
+	Name   graphql.String `json:"name,omitempty"`
+	Gender graphql.String `json:"gender,omitempty"`
 }
 
-func (p *Pokemon) Render(w http.ResponseWriter, r *http.Request) error {
+func (p *Character) Render(w http.ResponseWriter, r *http.Request) error {
 	return nil
 }
 
@@ -37,12 +39,15 @@ func main() {
 	router := chi.NewRouter()
 	router.Use(middleware.Logger)
 
-	// Routes
+	// User Routes
 	router.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello World!"))
 	})
 	router.Get("/users/{id}/{username}/*", getUser)
 	router.Post("/users", createUser)
+
+	// Character Routes
+	router.Get("/rick-morty/{id}", getCharacter)
 
 	http.ListenAndServe(":3000", router)
 }
@@ -60,11 +65,38 @@ func getUser(w http.ResponseWriter, r *http.Request) {
 
 func createUser(w http.ResponseWriter, r *http.Request) {
 	data := &User{}
-	if err := render.Bind(r, data); err != nil {
-		log.Print(err.Error())
-		render.Render(w, r, data)
-		return
+	/*
+		if err := render.Bind(r, data); err != nil {
+			log.Print(err.Error())
+			render.Render(w, r, data)
+			return
+		}
+	*/
+
+	err := json.NewDecoder(r.Body).Decode(&data)
+	if err != nil {
+		w.Write([]byte(err.Error()))
 	}
 
 	render.Render(w, r, data)
+}
+
+func getCharacter(w http.ResponseWriter, r *http.Request) {
+	// GraphQL Client
+	client := graphql.NewClient("https://rickandmortyapi.com/graphql", nil)
+
+	var query struct {
+		Character Character `graphql:"character(id: $id)"`
+	}
+
+	variables := map[string]interface{}{
+		"id": chi.URLParam(r, "id"),
+	}
+
+	err := client.Query(r.Context(), &query, variables)
+	if err != nil {
+		log.Printf("error: %v", err)
+	}
+
+	render.Render(w, r, &query.Character)
 }
